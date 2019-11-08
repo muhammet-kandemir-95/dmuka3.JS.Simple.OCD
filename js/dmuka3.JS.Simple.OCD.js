2cff202a647aaef8f4167f3e442f6e39d00b9fdb
muhammet-kandemir-95 <k8a1n6d3e3m7i11r4@hotmail.com>
2019-11-08 19:27:54 +0300

    Fixed IE bug

--- js/dmuka3.JS.Simple.OCD.js
+++ js/dmuka3.JS.Simple.OCD.js
@@ -107,6 +107,18 @@
 	});
 	//#endregion
 
+	// For IE.
+	if (Function.prototype.name === undefined && Object.defineProperty !== undefined) {
+		Object.defineProperty(Function.prototype, 'name', {
+			get: function () {
+				var funcNameRegex = /function\s([^(]{1,})\(/;
+				var results = (funcNameRegex).exec((this).toString());
+				return (results && results.length > 1) ? results[1].trim() : "";
+			},
+			set: function (value) { }
+		});
+	}
+
 	//#region Check Variable by ?
 	/**
 	 * Is variable's type boolean?
