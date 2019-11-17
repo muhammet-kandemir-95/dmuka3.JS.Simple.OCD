b879e46766dd9dc8d1041573eb94ab69ab9da473
muhammet-kandemir-95 <k8a1n6d3e3m7i11r4@hotmail.com>
2019-11-17 14:53:53 +0300

    Added computed css

--- js/dmuka3.JS.Simple.OCD.js
+++ js/dmuka3.JS.Simple.OCD.js
@@ -146,6 +146,17 @@
 						}
 					};
 				},
+				get computedCss () {
+					return function () {
+						var computed = window.getComputedStyle(self, null);
+
+						if (arguments.length === 1) {
+							return computed[arguments[0]];
+						} else {
+							return computed;
+						}
+					}
+				},
 				get data () {
 					return function () {
 						if (arguments.length === 1) {
@@ -319,7 +330,11 @@
 				},
 				get seturl () {
 					return function (url) {
-						history.replaceState(null, null, url);
+						if (checkVariableIsNullOrUndefined(history.replaceState) === false) {
+							history.replaceState(null, null, url);
+						} else {
+							location.href = url;
+						}
 					};
 				},
 				get cookie () {
@@ -432,6 +447,44 @@
 			set: function (value) { }
 		});
 	}
+
+	window.getComputedStyle = window.getComputedStyle || function (element) {
+		return element.currentStyle;
+	};
+
+	if (!String.prototype.padStart) {
+		String.prototype.padStart = function padStart (targetLength, padString) {
+			targetLength = targetLength >> 0; //truncate if number, or convert non-number to 0;
+			padString = String(typeof padString !== 'undefined' ? padString : ' ');
+			if (this.length >= targetLength) {
+				return String(this);
+			} else {
+				targetLength = targetLength - this.length;
+				if (targetLength > padString.length) {
+					padString += padString.repeat(targetLength / padString.length); //append to original to ensure we are longer than needed
+				}
+				return padString.slice(0, targetLength) + String(this);
+			}
+		};
+	}
+
+	if (!String.prototype.padEnd) {
+		String.prototype.padEnd = function padEnd (targetLength, padString) {
+			targetLength = targetLength >> 0; //floor if number or convert non-number to 0;
+			padString = String((typeof padString !== 'undefined' ? padString : ' '));
+			if (this.length > targetLength) {
+				return String(this);
+			}
+			else {
+				targetLength = targetLength - this.length;
+				if (targetLength > padString.length) {
+					padString += padString.repeat(targetLength / padString.length); //append to original to ensure we are longer than needed
+				}
+				return String(this) + padString.slice(0, targetLength);
+			}
+		};
+	}
+
 	if (!('remove' in Element.prototype)) {
 		$oldBrowser = true;
 		Element.prototype.remove = function () {
